bd76c5baae125041c48b809a82228e6a62639322
ConfidentProgrammer <deepp2618@gmail.com>
2020-12-24 18:49:13 -0500

    all new started

--- main.js
+++ main.js
@@ -42,4 +42,19 @@ tlName.from(myName, .1, {
 })
 
 
+//handling the details tag
+const All_Details = document.querySelectorAll('details');
+
+All_Details.forEach(deet=>{
+  deet.addEventListener('toggle', toggleOpenOneOnly)
+})
+
+function toggleOpenOneOnly(e) {
+  if (this.open) {
+    All_Details.forEach(deet=>{
+      if (deet!=this && deet.open) deet.open = false
+    });
+  }
+}
+
 
